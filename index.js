95778361d4f9f1dd9914b09b35d64e0e7dd6ae9d
Indira Briggiler <indira@eudaimonia.com.ar>
2019-01-04 18:34:44 -0300

    web created

---
+++ index.js
@@ -0,0 +1,9 @@
+$('.spanishT').on('click', function () {
+  $('.spanish').removeClass('hidden');
+  $('.english').addClass('hidden');
+})
+
+$('.englishT').on('click', function () {
+  $('.english').removeClass('hidden');
+  $('.spanish').addClass('hidden');
+})
